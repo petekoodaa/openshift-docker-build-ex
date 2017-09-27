9952d38c32732faa579140d6ffc26416dfd817c3
Petteri Lapinlampi <Petteri.Lapinlampi@gmail.com>
2017-09-27 14:38:15 +0300

    Use unbuffered stdout in python

--- hello.py
+++ hello.py
@@ -1,10 +1,8 @@
 import time
-import sys
 
 def main():
     while True:
-        sys.stdout.write("Hello OpenShift!\n")
-        sys.stdout.flush()
+        print("Hello OpenShift!")
         time.sleep(5)
 
 if __name__ == '__main__':
